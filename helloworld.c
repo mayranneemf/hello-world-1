4d75e88ea95c06e9a54f6f7ff347ec95acfc5d86
cadubentzen <cadubentzen@gmail.com>
2017-10-02 09:12:06 -0300

    .gitignore and modification in helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Hello World\n")
-    printf("Hello World 2\n")
+    printf("Hello World\n");
+    printf("Hello World 2\n");
 
     return 0;
 }
